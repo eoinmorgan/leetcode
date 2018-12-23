1408e740692186a23a9922a883bf38ba715d77af
Eoin Morgan <eoinbmorg@gmail.com>
2018-12-22 17:05:53 -0800

    #17 Letter Combinations of a Phone Number

--- letters_of_phone_number/letters_of_phone_number.py
+++ letters_of_phone_number/letters_of_phone_number.py
@@ -1,26 +1,26 @@
 nums_to_letters = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz"
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz"
 }
 
 def letter_combinations(digits):
     memo = {}
 
     if len(digits) == 0:
-        return ''
+        return []
 
     if digits in memo:
         return memo[digits]
 
     cur_digit_letters = [letter for letter in nums_to_letters[digits[0]]]
 
-    if len(digits == 1):
+    if len(digits) == 1:
         memo[digits] = cur_digit_letters
         return cur_digit_letters
 
@@ -29,10 +29,10 @@ def letter_combinations(digits):
     res = []
     for letter in cur_digit_letters:
         for tail in rest:
-            res.append(letter.join(tail))
+            res.append(letter + tail)
 
-    memo[digits] = tail
-    return tail
+    memo[digits] = res
+    return res
 
     
 
--- letters_of_phone_number/test_letters_of_phone_number.py
+++ letters_of_phone_number/test_letters_of_phone_number.py
@@ -7,5 +7,10 @@ class Test_Letters_of_phone_number(unittest.TestCase):
         expected = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
         self.assertEqual(letter_combinations(digits), expected)
 
+    def test_empty_input(self):
+        digits = ""
+        expected = []
+        self.assertEqual(letter_combinations(digits), expected)
+
 if __name__ == '__main__':
     unittest.main()
